422ac77e85eeb9203305e63b6e722b31c5e44461
sinka27 <sinka27392@gmail.com>
2022-03-11 01:21:37 -0800

    1. Updated score model to save username 2. Fixed API to post scores so that it saves username as well 3. Created API to return top 3 scores based on game value

--- server/models/Score.js
+++ server/models/Score.js
@@ -3,15 +3,17 @@ const { Schema, model } = require('mongoose');
  const scoreSchema = new Schema({
      score:{
          type:Number,
+         required: true
      },
      gamevalue:{
          type: Number,
          required: true
-
+     },
+     username:{
+        type: String,
+        required: true
      }
-   
  }, { timestamps: true })
-
 
 const Score = model('Score', scoreSchema);
 
